f07604ee0c10de3d9e8c0fb61e0996c22bd3c7d6
Saurn98 <87410763+Saurn98@users.noreply.github.com>
2022-05-29 02:43:30 +0530

    Product fully done yayyy

--- JS/product-types.js
+++ JS/product-types.js
@@ -1,14 +1,18 @@
 var backToTop = document.getElementById("back_to_top")
+var container_ka_baap = document.getElementById("container_ka_baap");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY < 200) {
         backToTop.style.display = "none";
+        container_ka_baap.style.display = "none";
     } else {
         backToTop.style.display = "block";
+        container_ka_baap.style.display = "block";
     }
     });
 
-backToTop.addEventListener("click", () => {
+function scrollToTop()
+{
     document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-});
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
