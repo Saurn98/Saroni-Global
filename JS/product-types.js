69c66ecd0cbc3230cf294b780d9e0118bc623f1e
Saurn98 <87410763+Saurn98@users.noreply.github.com>
2022-06-12 14:17:02 +0530

    Basmati Rice with images also done:)

--- JS/product-types.js
+++ JS/product-types.js
@@ -1,13 +1,11 @@
-var backToTop = document.getElementById("back_to_top")
+var backToTop = document.getElementById("back_to_top");
 var container_ka_baap = document.getElementById("container_ka_baap");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY < 200) {
         backToTop.style.display = "none";
-        container_ka_baap.style.display = "none";
     } else {
         backToTop.style.display = "block";
-        container_ka_baap.style.display = "block";
     }
     });
 
@@ -16,3 +14,5 @@ function scrollToTop()
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+const countAll = document.querySelectorAll('.scroll-buttons').length;
+console.log(countAll); 
