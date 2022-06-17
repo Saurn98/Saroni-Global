fb38b4fedf526b7d3d60dcc087367f90a8aef7d6
Saurn98 <87410763+Saurn98@users.noreply.github.com>
2022-06-18 00:36:39 +0530

    About us done 2022

--- JS/about.js
+++ JS/about.js
@@ -1,13 +1,15 @@
 width = screen.width; height = screen.height;
+console.log(width, height);
 
-      let margin_score = width - 1446;
-      console.log("magin_score: " + margin_score);
+let margin_score = width - 1300;
+console.log("magin_score: " + margin_score);
 
-      margin_to_set = "margin: -" + margin_score + " auto";
-      console.log(margin_to_set)
+margin_to_set = "margin: -" + margin_score + " auto";
+console.log(margin_to_set)
 
-      about_image = document.getElementById('about_image');
-      console.log(about_image)
+about_image = document.getElementById('about_image');
+console.log(about_image)
 
-      about_image.style.marginTop = "-" + margin_score + "px";
-      about_image.style.marginBottom = "auto";
+about_image.style.marginTop = "-" + margin_score + "px";
+about_image.style.marginBottom = "auto";
+
